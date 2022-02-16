12f8475157f62fb664e34be1231694c09a871d67
Maruf <alenmaruf.am@gmail.com>
2022-02-16 20:26:51 +0600

    Number Manipulation and F Strings in Python

---
+++ Day -02/num_mani&f_str.py
@@ -0,0 +1,14 @@
+print(type(8//3))
+
+result = 4/2
+result /=2
+
+print(result)
+
+
+score = 0
+height = 1.8
+isWinning = True
+
+#f-String
+print(f"your score is {score},your height is {1.8},You are winning is {isWinning}")
