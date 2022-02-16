2564929d945924afc957d1f751f0da21e2c2b7b9
Maruf <alenmaruf.am@gmail.com>
2022-02-16 11:16:26 +0600

    Input Function

---
+++ Day -01/input_function.py
@@ -0,0 +1,8 @@
+#input() will get user input in console
+#Then print() will print the word "Hello" and the user input 
+
+print("hello Ma" + input("What is your name?\n")+"!")
+
+#find the length of given name
+
+print(len(input("What is your name?\n")))
