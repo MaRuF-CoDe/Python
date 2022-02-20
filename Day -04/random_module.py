d2a337b32825be47851af6d41e3e05fe7d443d75
Maruf <alenmaruf.am@gmail.com>
2022-02-20 08:28:14 +0600

    Random Module

---
+++ Day -04/random_module.py
@@ -0,0 +1,13 @@
+import random
+
+random_int = random.randint(1,10)
+
+print(random_int)
+
+random_float = random.random() * 5
+
+print(random_float)
+
+lov_cal = random.randint(1,100)
+
+print(f"Your love calculator score is {lov_cal}")
