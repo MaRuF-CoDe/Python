be27f9f0ede98a806270eecf2b6698785c199c23
Maruf <alenmaruf.am@gmail.com>
2022-02-22 20:52:39 +0600

    Caesar Cipher Part 4 - User Experience Improvements & Final Touches

--- Day -08/Project_Caesar_Cipher.py
+++ Day -08/Project_Caesar_Cipher.py
@@ -1,18 +1,31 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y','z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-   
 def chesor(plain_text, shift_amount,direction):
     cipher_text = ""
     if direction == "decode":
         shift_amount *= -1
-    for letter in plain_text:
-        num_letter = alphabet.index(letter)
-        total_shift = num_letter + shift_amount
-        new_letter = alphabet[total_shift]
-        cipher_text += new_letter
+    for char in plain_text:
+        if char in alphabet:
+            num_letter = alphabet.index(char)
+            total_shift = num_letter + shift_amount
+            cipher_text += alphabet[total_shift]
     print(f"The {direction}d text is {cipher_text}")
 
-chesor(text,shift,direction)
+
+
+from art import logo
+print(logo)
+
+should_end =False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    chesor(text,shift,direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
