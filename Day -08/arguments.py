0ecdd89de0a380101410dae27a66544c538b2c62
Maruf <alenmaruf.am@gmail.com>
2022-02-22 17:14:13 +0600

    Functions with Inputs

---
+++ Day -08/arguments.py
@@ -0,0 +1,6 @@
+def greet(name,location): #Here name is parameter
+    print(f"Hello!{name} from {location}")
+    print(f"Hi! {name}")
+    print(f"Bye {name}")
+# greet("Maruf","Sabalia") #Here the value of parameter is the Argument
+greet(location = "Sabalis",name = "Maruf")
