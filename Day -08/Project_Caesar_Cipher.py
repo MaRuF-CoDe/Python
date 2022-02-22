1c12da44c35aa4808046e998e54b4fc1db0c7e89
Maruf <alenmaruf.am@gmail.com>
2022-02-22 20:35:08 +0600

    Caesar Cipher Part 3 - Reorganising our Code

--- Day -08/Project_Caesar_Cipher.py
+++ Day -08/Project_Caesar_Cipher.py
@@ -3,27 +3,16 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
-
-
-def encrypt(plain_text, shift_amount):
+   
+def chesor(plain_text, shift_amount,direction):
     cipher_text = ""
+    if direction == "decode":
+        shift_amount *= -1
     for letter in plain_text:
         num_letter = alphabet.index(letter)
         total_shift = num_letter + shift_amount
         new_letter = alphabet[total_shift]
         cipher_text += new_letter
-    print(f"The encoded text is {cipher_text}")
-   
-def decrypt(plain_text, shift_amount):
-    cipher_text = ""
-    for letter in plain_text:
-        num_letter = alphabet.index(letter)
-        total_shift = num_letter - shift_amount
-        new_letter = alphabet[total_shift]
-        cipher_text += new_letter
-    print(f"The decoded text is {cipher_text}")
+    print(f"The {direction}d text is {cipher_text}")
 
-if direction == "encode":
-    encrypt(text, shift)
-elif direction == "decode":
-    decrypt(text,shift)
+chesor(text,shift,direction)
