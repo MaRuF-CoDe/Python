4bfaea64f018ed14ff77f9bc4608b6503dc0dd4b
Maruf <alenmaruf.am@gmail.com>
2022-02-23 10:39:28 +0600

    Nesting Lists and Dictionaries

---
+++ Day -09/nested_dic.py
@@ -0,0 +1,22 @@
+# Nested dictionary in a dictionary
+
+travel_log ={
+    "France": { "cities_visited":["Paris","Lille","Dijon"],"total_visited":12},
+    "Germany": { "cities_visited":["Berlin","Humburg","Stuartburg"],"total_visited":12},
+} 
+
+# nested dictionary in a  list
+
+
+travel_log ={
+    {
+        "country":"France",
+        "cities_visited":["Paris","Lille","Dijon"],
+        "total_visited":12
+    },
+    {
+        "country":"Germany",
+        "cities_visited":["Berlin","Humburg","Stuartburg"],
+        "total_visited":12
+    },
+} 
