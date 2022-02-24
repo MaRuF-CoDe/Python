826b62e567d6b13a30591cebf7a0cc6403bb21b1
Maruf <alenmaruf.am@gmail.com>
2022-02-24 10:17:48 +0600

    Calculator Part 1_ Combining Dictionaries and Functions

---
+++ Day -10/calculator.py
@@ -0,0 +1,38 @@
+#calculation
+
+#Add
+def add(n1,n2):
+    return n1 + n2
+
+#substraction
+def sub(n1,n2):
+    return n1 - n2
+
+#Multiplication
+def mul(n1,n2):
+    return n1 * n2
+
+#Division
+def div(n1,n2):
+    return n1 / n2
+
+operations = {
+    '+' : add,
+    '-' : sub,
+    '*' : mul,
+    '/' : div
+}
+
+num1 = int(input("What's the first number?: "))
+
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+
+num2 = int(input("What's the second number?: "))
+
+calculation_function = operations[operation_symbol]
+
+answer = calculation_function(num1,num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
