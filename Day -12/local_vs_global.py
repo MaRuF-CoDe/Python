fc9f17c3c1472a95a5effab0c18e6724d3db9b8c
Maruf <alenmaruf.am@gmail.com>
2022-02-24 16:19:23 +0600

    Does Python Have Block Scope_

--- Day -12/local_vs_global.py
+++ Day -12/local_vs_global.py
@@ -26,3 +26,19 @@ def game():
 
     drink_potion()
 print(player_health)
+
+# There is no Block Scope
+
+# game_level = 3
+# enemies = ["Skeleton", "Zombie", "Alien"]
+# if game_level < 5:
+#     new_enemy = enemies[0]
+# print(new_enemy)
+
+game_level = 3
+def create_enemy():
+    enemies = ["Skeleton", "Zombie", "Alien"]
+    if game_level < 5:
+        new_enemy = enemies[0]
+    print(new_enemy)
+create_enemy()
