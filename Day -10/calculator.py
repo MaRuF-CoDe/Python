7b3dbdfb3a65836eb1179471ad98637b6dbf6ea6
Maruf <alenmaruf.am@gmail.com>
2022-02-24 10:35:21 +0600

    While Loops, Flags and Recursion

--- Day -10/calculator.py
+++ Day -10/calculator.py
@@ -22,17 +22,28 @@ operations = {
     '*' : mul,
     '/' : div
 }
+def calculation():
+    num1 = int(input("What's the first number?: "))
 
-num1 = int(input("What's the first number?: "))
+    should_go = True
 
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
+    while should_go :
+        for symbol in operations:
+            print(symbol)
+        operation_symbol = input("Pick an operation from the line above: ")
 
-num2 = int(input("What's the second number?: "))
+        num2 = int(input("What's the next number?: "))
 
-calculation_function = operations[operation_symbol]
+        calculation_function = operations[operation_symbol]
 
-answer = calculation_function(num1,num2)
+        answer = calculation_function(num1,num2)
 
-print(f"{num1} {operation_symbol} {num2} = {answer}")
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"to more calculation with {answer} type y or to start new type n :")=='y':
+            num1 = answer
+        else:
+            should_go = False
+            calculation()
+
+calculation()
