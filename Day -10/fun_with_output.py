170177d01dba43ffab0ce8c58dcd22f8ba2bd31b
Maruf <alenmaruf.am@gmail.com>
2022-02-24 09:26:15 +0600

    Functions with Outputs

---
+++ Day -10/fun_with_output.py
@@ -0,0 +1,6 @@
+def format(f_name,l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+print(format("mARuF","PPPP"))
