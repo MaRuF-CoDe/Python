21fcb1a43f42e9406cd3456f048fbdc0e21dc875
Maruf Bangabashi <alenmaruf.am@gmail.com>
2022-02-28 21:29:39 +0600

    Constructing Objects and Accessing their Attributes and Methods

---
+++ Day -16/cons_obj.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()#create a new object from a blueprint/class
+timmy.shape("turtle")#we can call methods that are associated with the object
+timmy.color("blue")#we can call methods that are associated with the object
+timmy.forward(100)#we can call methods that are associated with the object
+
+
+my_screen = Screen() #create a new object from a blueprint/class
+print(my_screen.canvheight) #tap into attribute by using obeject dot(.) name of the atrribute
+my_screen.exitonclick() #we can call methods that are associated with the object
