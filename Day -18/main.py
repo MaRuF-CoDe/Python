7ab4120334d3a550d6a453984c73bbb2cbfca6bb
Maruf Bangabashi <alenmaruf.am@gmail.com>
2022-03-03 17:25:52 +0600

    Turtle Challenge 2 - Draw a Dashed Line

--- Day -18/main.py
+++ Day -18/main.py
@@ -4,12 +4,22 @@ turtle = Turtle()
 turtle.shape("turtle")
 turtle.color("#285078")
 
-for _ in range(4):
-    turtle.forward(100)
-    turtle.left(90)
+# To Draw a Square 
+# for _ in range(4):
+#     turtle.forward(10)
+#     # turtle.left(90)
 
 
+# For Dashed Line
 
+for _ in range (15):
+    # pick the pen up to leave white space between dashes
+    turtle.forward(10)
+    turtle.penup()
+    # put the pen down and draw the second dash
+    turtle.forward(10)
+    turtle.pendown()
+    
 
 screen = Screen()
 screen.exitonclick()
